2e18de7aed0ae82007bfe9260ada07af3c77bfe5
alex darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-11-03 02:23:43 -0500

    Update main.py

--- HW2_pruning/main.py
+++ HW2_pruning/main.py
@@ -317,6 +317,13 @@ def masked_retrain(model, masks, optimizer, train_loader, test_loader, criterion
             acc = test(model, device, test_loader)
             print(f"Epoch {i} training accuracy: {acc}")
 
+        model.eval()
+        for name, param in model.named_parameters():
+            if name in masks:
+                mask = masks[name]
+                param.data.mul_(mask)
+                acc = test(model, device, test_loader)
+                print(f"Epoch {i} training accuracy: {acc}")
         model.train()
         for inputs, targets in train_loader:
             inputs, targets = inputs.to(device), targets.to(device)
