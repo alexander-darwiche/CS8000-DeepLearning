c53141da1df6bc648d0dc5d5a2760662ed6421ec
alex darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-11-05 22:11:59 -0500

    Update viz.py

--- HW2_pruning/viz.py
+++ HW2_pruning/viz.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Directory with model files
-model_dir = "model"
+model_dir = "./model"
 
 # Regex to extract sparsity and accuracy from filename
 pattern = re.compile(r".*_(imp|omp)_(\d+\.\d+)_acc_(\d+\.\d+)\.pt")
